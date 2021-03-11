e36b31f72638d5d12d28276bea201cc0ecccbd87
FilipeLageGarcia <filipelagegarcia@gmail.com>
2021-03-11 02:09:49 -0300

    completado

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+          string[] valores = Console.ReadLine().Split();
+
+          double coodernadaX = double.Parse(valores[0]);
+          double coodernadaY = double.Parse(valores[1]);
+
+          if ( (coodernadaX > 0)&&(coodernadaY > 0))
+          {
+              Console.WriteLine("Q1");
+          }
+          else if ( (coodernadaX > 0)&&(coodernadaY < 0))
+          {
+              Console.WriteLine("Q4");
+          }
+          else if ( (coodernadaX < 0)&&(coodernadaY < 0))
+          {
+              Console.WriteLine("Q3");
+          }
+          else if ( (coodernadaX < 0)&&(coodernadaY > 0))
+          {
+              Console.WriteLine("Q2");
+          }
+          else if ( (coodernadaX == 0)&&!(coodernadaY == 0))
+          {
+              Console.WriteLine("Eixo Y");
+          }
+          else if ( !(coodernadaX == 0)&&(coodernadaY == 0))
+          {
+              Console.WriteLine("Eixo X");
+          }
+          else if ( (coodernadaX == 0)&&(coodernadaY == 0))
+          {
+              Console.WriteLine("Origem");
+          }
+ 
+    }
+
+}
